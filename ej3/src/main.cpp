247577ba9fd108449b2179a3f18efe9ef85b175d
Hernan Fuentes Araujo <hernaneg350@gmail.com>
2017-05-28 22:09:01 -0300

    Fix method input/output format

--- ej3/src/main.cpp
+++ ej3/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <tuple>
 #include <string>
+#include <list>
 #include "DisjointSet.h"
 #include "util.h"
 
@@ -15,11 +16,7 @@ bool sortConstructionQueue (tuple<int, int, int> a, tuple<int, int, int> b) {
     return get<2>(a) < get<2>(b);
 }
 
-int main() {
-    string nString;
-    getline(cin, nString);
-    int n = stoi(nString);
-
+void run_solution(int n) {
     vector<tuple<int, int, int>> constructionQueue;
     vector<tuple<int, int, int>> destructionQueue;
 
@@ -34,15 +31,11 @@ int main() {
         int isPresent = (bool)stoi(trim(edgeData[2]));
         int cost = stoi(trim(edgeData[3]));
 
-        vector<tuple<int, int, int>>* queue;
-
         if (isPresent) {
-            queue = &destructionQueue;
+            destructionQueue.push_back(make_tuple(c1, c2, cost));
         } else {
-            queue = &constructionQueue;
+            constructionQueue.push_back(make_tuple(c1, c2, cost));
         }
-
-        queue->push_back(make_tuple(c1, c2, cost));
     }
 
     sort(constructionQueue.begin(), constructionQueue.end(), sortConstructionQueue);
@@ -50,6 +43,7 @@ int main() {
 
     DisjointSet UDS(n);
     int min = 0;
+    list<tuple<int, int>> routes;
 
     for (vector<tuple<int, int, int>>::iterator it = destructionQueue.begin(); it != destructionQueue.end(); ++it) {
         int v1 = get<0>(*it);
@@ -57,6 +51,7 @@ int main() {
 
         if (UDS.find(v1) != UDS.find(v2)) {
             UDS.setUnion(v1, v2);
+            routes.push_back(make_tuple(v1, v2));
         } else {
             int cost = get<2>(*it);
             min += cost;
@@ -71,8 +66,39 @@ int main() {
             int cost = get<2>(*it);
             min += cost;
             UDS.setUnion(v1, v2);
+            routes.push_back(make_tuple(v1, v2));
         }
     }
 
-    cout << min << endl;
+    cout << min << " " << routes.size() << " ";
+
+    for (list<tuple<int, int>>::iterator routeIt = routes.begin(); routeIt != routes.end(); ++routeIt)
+    {
+        tuple<int, int> route = *routeIt;
+        cout << "c" << get<0>(route) << "1 " << "c" << get<1>(route) << "2 ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    bool keepReading = true;
+    while (keepReading)
+    {
+        string instanceSizeString;
+        getline(cin, instanceSizeString);
+
+        vector<string> instanceSize = split(instanceSizeString, ' ');
+
+        int n = stoi(trim(instanceSize[0]));
+
+        if (n == -1)
+        {
+            keepReading = false;
+            break;
+        }
+
+        run_solution(n);
+    }
 }
